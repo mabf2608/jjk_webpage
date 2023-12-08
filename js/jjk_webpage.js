2f03790913069bf7a00ae2d8d10b75a38bf26d68
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-12-09 00:56:31 +0100

    Segunda subida

--- js/jjk_webpage.js
+++ js/jjk_webpage.js
@@ -16,8 +16,8 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-modal.style.display = "block";
-  }
+  modal.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
 modal.style.display = "none";
@@ -27,4 +27,4 @@ window.onclick = function(event) {
 if (event.target == modal) {
   modal.style.display = "none";
   }
-} 
+}
